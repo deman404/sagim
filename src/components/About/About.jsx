bdbb90cbc749b1864aea7ac6bda624315eb7c385
deman404 <mr.aymanazhar.m@gmail.com>
2024-10-06 22:02:33 +0100

    v3

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./About.css";
 import useWindowSize from "../../Hooks/useWindowSize";
+import Card2 from "../tools/card2";
+import about from '../../assets/images/about-1-1.png';
 function About() {
   const size = useWindowSize();
   return (
     <>
-      <div className="Top2">
+      <div className="Top3">
         <div className="title-container">
           <h1>
             About Us <span className="underline"></span>
@@ -13,9 +15,16 @@ function About() {
           <p>Les filières et diplômes FEDE</p>
         </div>
         <div
-          className="cardsView"
+          className="cardsView2"
           style={{ display: size.width > 800 ? "flex" : "block" }}
-        ></div>
+        >
+          <div className="leftSide">
+            <div className="ImageBorders">
+                <img src={about} alt="about" style={{width:300}}/>
+            </div>
+          </div>
+          
+        </div>
       </div>
     </>
   );
